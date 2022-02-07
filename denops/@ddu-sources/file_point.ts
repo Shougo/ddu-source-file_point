5c6c52292f927b67e97ffea5e9f8723c7ee54208
Shougo Matsushita <Shougo.Matsu@gmail.com>
2022-02-07 11:50:29 +0900

    Parse jump line

--- denops/@ddu-sources/file_point.ts
+++ denops/@ddu-sources/file_point.ts
@@ -4,6 +4,7 @@ import {
 } from "https://deno.land/x/ddu_vim@v0.7.2/types.ts#^";
 import { Denops, fn } from "https://deno.land/x/ddu_vim@v0.7.2/deps.ts#^";
 import { ActionData } from "https://deno.land/x/ddu_kind_file@v0.1.0/file.ts#^";
+import { isAbsolute, join } from "https://deno.land/std@0.123.0/path/mod.ts";
 
 type Params = Record<string, never>;
 
@@ -18,10 +19,10 @@ export class Source extends BaseSource<Params> {
   }): Promise<void> {
     this.line = await fn.getline(args.denops, ".");
 
-    // Ignore expand() errors
     try {
       this.cfile = await fn.expand(args.denops, "<cfile>") as string;
-    } catch (e: unknown) {
+    } catch (_: unknown) {
+      // Ignore expand() errors
     }
   }
 
@@ -30,15 +31,62 @@ export class Source extends BaseSource<Params> {
     sourceParams: Params;
   }): ReadableStream<Item<ActionData>[]> {
     const cfile = this.cfile;
+    const line = this.line;
 
-    console.log(cfile);
     return new ReadableStream({
       async start(controller) {
-        if (new RegExp("^https?://").test(cfile)) {
+        const cwd = await fn.getcwd(args.denops) as string;
+        const re = /^.*?([^: ]+):(\d+)(?::(\d+))?/;
+        const matched = line.match(re);
+
+        const exists = async (filename: string) => {
+          try {
+            await Deno.stat(filename);
+            // successful, file or directory must exist
+            return true;
+          } catch (_) {
+            return false;
+          }
+        };
+
+        if (matched) {
+          const parseMatched = (
+            ary: string[],
+            index: number,
+            def: number | string,
+          ) => {
+            return ary[index] ? ary[index] : def;
+          };
+
+          const path = parseMatched(matched, 1, "") as string;
+          const fullPath = isAbsolute(path) ? path : join(cwd, path);
+
+          // Only exists the file
+          if (await exists(fullPath)) {
+            controller.enqueue([
+              {
+                word: matched[0],
+                action: {
+                  path: fullPath,
+                  lineNr: Number(parseMatched(matched, 2, 0)),
+                  col: Number(parseMatched(matched, 3, 0)),
+                },
+              },
+            ]);
+          }
+        }
+
+        const fullPath = isAbsolute(cfile) ? cfile : join(cwd, cfile);
+        if (await exists(fullPath)) {
           controller.enqueue(
-            [{ word: cfile, action: { path: cfile }}]
+            [{ word: cfile, action: { path: fullPath } }],
+          );
+        } else if (new RegExp("^https?://").test(cfile)) {
+          controller.enqueue(
+            [{ word: cfile, action: { path: cfile } }],
           );
         }
+
         controller.close();
       },
     });
