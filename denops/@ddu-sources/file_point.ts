81ec776080bd06b1dffbabab721762958f1251dc
Shougo Matsushita <Shougo.Matsu@gmail.com>
2022-02-10 20:15:00 +0900

    Update

--- denops/@ddu-sources/file_point.ts
+++ denops/@ddu-sources/file_point.ts
@@ -1,10 +1,10 @@
 import {
   BaseSource,
   Item,
-} from "https://deno.land/x/ddu_vim@v0.7.2/types.ts#^";
-import { Denops, fn } from "https://deno.land/x/ddu_vim@v0.7.2/deps.ts#^";
-import { ActionData } from "https://deno.land/x/ddu_kind_file@v0.1.0/file.ts#^";
-import { isAbsolute, join } from "https://deno.land/std@0.123.0/path/mod.ts";
+} from "https://deno.land/x/ddu_vim@v0.10.0/types.ts";
+import { Denops, fn } from "https://deno.land/x/ddu_vim@v0.10.0/deps.ts";
+import { ActionData } from "https://deno.land/x/ddu_kind_file@v0.2.0/file.ts";
+import { isAbsolute, join } from "https://deno.land/std@0.125.0/path/mod.ts";
 
 type Params = Record<string, never>;
 
@@ -54,7 +54,7 @@ export class Source extends BaseSource<Params> {
             index: number,
             def: number | string,
           ) => {
-            return ary[index] ? ary[index] : def;
+            return ary[index] ?? def;
           };
 
           const path = parseMatched(matched, 1, "") as string;
